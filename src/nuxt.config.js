79c8337724245d6edc017e37d77fdc958a5baef1
indexpro24@gmail.com <indexpro24@gmail.com>
2021-11-20 23:22:56 +0300

    git init

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -2,18 +2,18 @@
 export default {
   mode: 'universal',
   server: {
-    port: 1111,
+    port: 4170,
     host: '0.0.0.0'
   },
   /*
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'expo-dom.com || Производители корпусной и мягкой мебели, поставщики корпусной и мягкой мебели, оказание услуг',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '"Экспо-ДОМ" это компания, объединяющая продавцов, производителей и поставщиков корпусно и мягкой мебели' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
