d7429a53f6da873646f437f22ccc5b98e4146462
indexpro24@gmail.com <indexpro24@gmail.com>
2021-11-20 23:11:17 +0300

    git init

---
+++ src/pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="h-screen">
+    <div class="relative overflow-hidden ">
+      <div class="relative pt-24 pb-16 sm:pb-24">
+        <main class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24">
+          <div class="text-center font-serif">
+            <h1 class="my-12 text-6xl tracking-tight  text-gray-900 xs:text-4xl md:text-7xl">EXPO-DOM.COM</h1>
+            <h1 class="text-4xl tracking-tight text-gray-900 sm:text-5xl md:text-7xl">
+              <span class="block xl:inline">Предварительная регистрация</span><br>
+              <span class="block text-indigo-600 xl:inline">с 1 января 2022</span>
+            </h1>
+            <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+              Современный агрегатор компаний для ваших клиентов
+            </p>
+            <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
+              <div class="rounded-md shadow">
+                <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
+                  Ресурс бесплатный
+                </a>
+              </div>
+              <!--            <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">-->
+              <!--              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10">-->
+              <!--                Live demo-->
+              <!--              </a>-->
+              <!--            </div>-->
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
